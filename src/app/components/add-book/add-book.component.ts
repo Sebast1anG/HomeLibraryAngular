a800f5a3751444454a967deb99b099809eabf6ea
Sebastian Garas <sebastian.garas66@gmail.com>
2023-11-08 18:59:47 +0100

    Publish App

---
+++ src/app/components/add-book/add-book.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { Book } from 'src/app/models/book.model';
+import { BookService } from 'src/app/services/book.service';
+
+@Component({
+  selector: 'app-add-book',
+  templateUrl: './add-book.component.html',
+  styleUrls: ['./add-book.component.css']
+})
+export class AddBookComponent {
+
+  book: Book = {
+    title: '',
+    category: '',
+    published: false
+  };
+  submitted = false;
+
+  constructor(private bookService: BookService) { }
+
+  saveBook(): void {
+    const data = {
+      title: this.book.title,
+      category: this.book.category
+    };
+
+    this.bookService.create(data)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.submitted = true;
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  newBook(): void {
+    this.submitted = false;
+    this.book = {
+      title: '',
+      category: '',
+      published: false
+    };
+  }
+
+}
